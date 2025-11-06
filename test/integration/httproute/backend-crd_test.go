70e1fd9151ee5eb9bf89e602946cbe547ab7ca53
Helene Durand <hdurand@haproxy.com>
2025-11-06 15:30:55 +0100

    TEST/MEDIUM: add integration tests for Backend CRD

---
+++ test/integration/httproute/backend-crd_test.go
@@ -0,0 +1,72 @@
+//
+// Copyright 2025 HAProxy Technologies LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package httproute
+
+import (
+	"path"
+
+	"github.com/haproxytech/haproxy-unified-gateway/test/integration/utils"
+)
+
+func (s *HTTPRouteTestSuite) Test_HTTPRoute_BackendCRD_basic() {
+	fixtureDirPath := utils.GetCRDFixturePath()
+	fixtureDir := "backend-crd"
+
+	fixturePath := path.Join(fixtureDirPath, fixtureDir, "basic")
+	s.CreateFixtures(fixturePath, nil)
+	defer s.CleanupFixtures(fixturePath, nil)
+
+	// Expected Conditions
+	expectationsPath := path.Join(fixturePath, "expectations")
+	expectedCondPath := path.Join(expectationsPath, "conditions.yaml")
+	expectedConditions := s.YamlToRouteConditions(expectedCondPath)
+
+	httpRouteName := "route-echo-backend-cr-basic"
+	s.expectConditionsUpdated(s.Test().Ctx, s.Test().Namespace, httpRouteName, expectedConditions)
+
+	// Check AttachedRoutes on Gateway status
+	s.expectAttachedRoute(s.Test().Ctx, s.Test().Namespace, "gateway", "http", 1)
+
+	// haproxy.cfg Backends
+	backendsExpectationsPath := path.Join(expectationsPath, "backends")
+	expectedBackends := []string{"link1_e2e-tests-httproute_http-echo_80_ef84fba452b3f62faf235c47b46c1e54"}
+	s.ExpectBackends(s.Test().Ctx, backendsExpectationsPath, expectedBackends)
+}
+
+func (s *HTTPRouteTestSuite) Test_HTTPRoute_BackendCRD_extended() {
+	fixtureDirPath := utils.GetCRDFixturePath()
+	fixtureDir := "backend-crd"
+
+	fixturePath := path.Join(fixtureDirPath, fixtureDir, "extended")
+	s.CreateFixtures(fixturePath, nil)
+	defer s.CleanupFixtures(fixturePath, nil)
+
+	// Expected Conditions
+	expectationsPath := path.Join(fixturePath, "expectations")
+	expectedCondPath := path.Join(expectationsPath, "conditions.yaml")
+	expectedConditions := s.YamlToRouteConditions(expectedCondPath)
+
+	httpRouteName := "route-echo-backend-cr-basic"
+	s.expectConditionsUpdated(s.Test().Ctx, s.Test().Namespace, httpRouteName, expectedConditions)
+
+	// Check AttachedRoutes on Gateway status
+	s.expectAttachedRoute(s.Test().Ctx, s.Test().Namespace, "gateway", "http", 1)
+
+	// haproxy.cfg Backends
+	backendsExpectationsPath := path.Join(expectationsPath, "backends")
+	expectedBackends := []string{"link1_e2e-tests-httproute_http-echo_80_870306e1a7d334a1eceb23475e6075aa"}
+	s.ExpectBackends(s.Test().Ctx, backendsExpectationsPath, expectedBackends)
+}
